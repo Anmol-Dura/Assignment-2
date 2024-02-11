7245d1ffb88732f0e769663bf8c14421f40ee6e3
Anmol Dura <anmoldura8@gmail.com>
2024-02-11 16:29:47 -0500

    finished the project

--- belongsOrNot/Program.cs
+++ belongsOrNot/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+Console.Write("Enter a real number : ");
+int userNumber = int.Parse(Console.ReadLine());
+if ((userNumber >= 2 && userNumber < 3) || (userNumber > 0 && userNumber <= 1) ||
+    (userNumber >= -10 && userNumber <= -2))
+{
+    Console.Write($" x belongs to I ... ");
+}
+else
+{
+    Console.Write(" X does not belong to I...");
+}
+    
